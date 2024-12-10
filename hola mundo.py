9827bd475d594d83c9fbd651b60cb12805a4b6f9
danivde <dvazquezd9@gmail.com>
2024-12-10 11:04:28 +0100

    primer ejercicio python

---
+++ hola mundo.py
@@ -0,0 +1,12 @@
+"""probas identacion en python"""
+A = 3
+if A == 1:
+    print ("A es 1")
+    print ("test A es 1")
+else :
+    print ("A no es 1")
+    print ("test a no es 1")
+
+print ("esto siempre imprime")
+
+
